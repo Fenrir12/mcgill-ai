e7234f5a264b514d2eea801fbd3826c09d009830
f3nr1r <sotistechno@gmail.com>
2017-09-17 20:59:34 -0400

    Fixed problems on player conditions and depth limit too deep

--- Assignment 1/dynamic-connect-4.py
+++ Assignment 1/dynamic-connect-4.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
 from time import time
+from random import shuffle
 from DynamicConnect4Interface import draw_table_score
+
+
 class game():
     def __init__(self):
         self.ROWS = 7
@@ -18,11 +21,15 @@ class game():
         self.MaxSym = 'X'
         self.MinSym = 'O'
         self.EmpSym = '_'
-        self.depth_limit = 6
+        self.depth_limit = 4
         self.time_limit = 20
         self.start_time = time()
+        self.MIN = -1000
+        self.MAX = 1000
+        self.no_of_nodes = 0
+        self.no_of_plies = []
 
-    # Method of evaluation function. \
+    # Method of winning check function. \
     # Returns 1000 for winning Maximizer and -1000 for winning Minimizer
     def is_winning(self, board):
         eval_value = 0
@@ -62,7 +69,7 @@ class game():
                     else:
                         return -10
 
-        # Assess vertical win (decreasing) for both players
+        # Assess diagonal win (decreasing) for both players
         for row in range(self.ROWS - 3, self.ROWS):
             for col in range(0, self.COLS - 3):
                 if board[row][col] != self.EmpSym \
@@ -73,6 +80,96 @@ class game():
                         return 10
                     else:
                         return -10
+
+        # Return value of evaluation function if no win
+        return self.eval(board)
+
+    # Method of evaluation function. Returns the goodness of a state
+    def eval(self, board):
+        # Assess vertical 3-in-a-row for both players
+        for row in range(0, self.ROWS - 2):
+            for col in range(0, self.COLS):
+                if board[row][col] != self.EmpSym \
+                        and board[row][col] == board[row + 1][col] \
+                        and board[row][col] == board[row + 2][col]:
+                    if board[row][col] == self.MaxSym:
+                        return 7
+                    else:
+                        return -7
+
+        # Assess horizontal 3-in-a-row for both players
+        for row in range(0, self.ROWS):
+            for col in range(0, self.COLS - 2):
+                if board[row][col] != self.EmpSym \
+                        and board[row][col] == board[row][col + 1] \
+                        and board[row][col] == board[row][col + 2]:
+                    if board[row][col] == self.MaxSym:
+                        return 7
+                    else:
+                        return -7
+
+        # Assess diagonal (increasing) 3-in-a-row for both players
+        for row in range(0, self.ROWS - 2):
+            for col in range(0, self.COLS - 2):
+                if board[row][col] != self.EmpSym \
+                        and board[row][col] == board[row + 1][col + 1] \
+                        and board[row][col] == board[row + 2][col + 2]:
+                    if board[row][col] == self.MaxSym:
+                        return 6
+                    else:
+                        return -6
+
+        # Assess diagonal (decreasing) 3-in-a-row for both players
+        for row in range(self.ROWS - 4, self.ROWS):
+            for col in range(0, self.COLS - 2):
+                if board[row][col] != self.EmpSym \
+                        and board[row][col] == board[row - 1][col + 1] \
+                        and board[row][col] == board[row - 2][col + 2]:
+                    if board[row][col] == self.MaxSym:
+                        return 6
+                    else:
+                        return -6
+
+        # Assess vertical 2-in-a-row for both players
+        for row in range(0, self.ROWS - 1):
+            for col in range(0, self.COLS):
+                if board[row][col] != self.EmpSym \
+                        and board[row][col] == board[row + 1][col]:
+                    if board[row][col] == self.MaxSym:
+                        return 5
+                    else:
+                        return -5
+
+        # Assess horizontal 2-in-a-row for both players
+        for row in range(0, self.ROWS):
+            for col in range(0, self.COLS - 1):
+                if board[row][col] != self.EmpSym \
+                        and board[row][col] == board[row][col + 1]:
+                    if board[row][col] == self.MaxSym:
+                        return 5
+                    else:
+                        return -5
+
+        # Assess diagonal (increasing) 2-in-a-row for both players
+        for row in range(0, self.ROWS - 1):
+            for col in range(0, self.COLS - 1):
+                if board[row][col] != self.EmpSym \
+                        and board[row][col] == board[row + 1][col + 1]:
+                    if board[row][col] == self.MaxSym:
+                        return 4
+                    else:
+                        return -4
+
+        # Assess diagonal (decreasing) 2-in-a-row for both players
+        for row in range(self.ROWS - 5, self.ROWS):
+            for col in range(0, self.COLS - 1):
+                if board[row][col] != self.EmpSym \
+                        and board[row][col] == board[row - 1][col + 1]:
+                    if board[row][col] == self.MaxSym:
+                        return 4
+                    else:
+                        return -4
+
         return 0
 
     # Method to find every possible moves
@@ -161,7 +258,6 @@ class game():
                 moves.append([self.ROWS - 1, self.COLS - 1, 'N'])
             if board[self.ROWS - 1][self.COLS - 2] == self.EmpSym:
                 moves.append([self.ROWS - 1, self.COLS - 1, 'W'])
-
         return moves
 
     # Do the move and return the board for this move
@@ -198,23 +294,23 @@ class game():
             board[move[0]][move[1] - 1] = self.EmpSym
         return board
 
+    # Minimax function to explore possible moves
     def minimax(self, board, depth, player):
-
-
+        # Increment node counter
+        self.no_of_nodes += 1
         # Look if someone player is winning
         score = self.is_winning(board)
-        if score == 10:
+        if score == 10 \
+                or score == -10 \
+                or depth > self.depth_limit \
+                or time() - self.start_time > self.time_limit:
             return score
-        if score == -10:
-            return score
-        if depth > self.depth_limit or time() - self.start_time > self.time_limit:
-            return 0
 
         # Run for maximizer's turn
         if player == self.MaxPlayer:
             # Gather valid moves for max player
             moves = game.move(board, self.MaxPlayer)
-            best = -1000
+            best = self.MIN
             # Do every possible move and run minimax recursively for each possible state at different depths
             for move in moves:
                 # Do the move and go into minimax for next depth
@@ -228,7 +324,7 @@ class game():
         else:
             # Gather valid moves for min player
             moves = game.move(board, self.MinPlayer)
-            best = 1000
+            best = self.MAX
             # Do every possible move and run minimax reccursively for each possible state at different depths
             for move in moves:
                 # Do the move and go into minimax for next depth
@@ -239,49 +335,55 @@ class game():
                 board = game.undo_move(board, move, player)
             return best
 
+    #Alpha-Beta Pruning variation of minimax fuinction
     def alphabeta(self, board, depth, player, alpha, beta):
 
 
         # Look if someone player is winning
         score = self.is_winning(board)
-        if score == 10:
-            return score
-        if score == -10:
-            return score
+
         if depth > self.depth_limit or time() - self.start_time > self.time_limit:
-            return 0
+            if player == self.MaxPlayer:
+                return alpha
+            else:
+                return beta
 
         # Run for maximizer's turn
         if player == self.MaxPlayer:
+            best = self.MIN
             # Gather valid moves for max player
             moves = game.move(board, self.MaxPlayer)
-            best = -1000
             # Do every possible move and run minimax recursively for each possible state at different depths
             for move in moves:
                 # Do the move and go into minimax for next depth
                 board = game.do_move(board, move, player)
-                best = max(best, game.alphabeta(board, depth + 1, not player), alpha, beta)
-
+                best = max(best, game.alphabeta(board, depth + 1, not player, alpha, beta))
+                alpha = max(best, alpha)
                 # Return board to previous state
                 board = game.undo_move(board, move, player)
+                if beta <= alpha:
+                    break
             return best
         # Run for minimizer's move
         else:
             # Gather valid moves for min player
             moves = game.move(board, self.MinPlayer)
-            best = 1000
+            best = self.MAX
             # Do every possible move and run minimax reccursively for each possible state at different depths
             for move in moves:
                 # Do the move and go into minimax for next depth
                 board = game.do_move(board, move, player)
-                best = min(best, game.alphabeta(board, depth + 1, not player), alpha, beta)
-
+                best = min(best, game.alphabeta(board, depth + 1, not player, alpha, beta))
+                beta = min(best, beta)
                 # Return board to previous state
                 board = game.undo_move(board, move, player)
+                if beta <= alpha:
+                    break
             return best
 
+    # Runs minimax at depth 0 to return best move at present
     def find_best_move(self, board, player):
-        if self.MaxPlayer:
+        if self.MaxPlayer == player:
             bestvalue = -1000
         else:
             bestvalue = 1000
@@ -292,12 +394,12 @@ class game():
         moves = game.move(board, player)
 
         for move in moves:
-            #board = game.do_move(board, move, player)
-            val_of_move = game.minimax(game.do_move(board, move, player), 0, not player)
-            #board = game.undo_move(board, move, player)
+            board = game.do_move(board, move, player)
+            val_of_move = game.minimax(board, 0, not player)
+            board = game.undo_move(board, move, player)
 
             # Get best move for min or max player and returns it
-            if self.MaxPlayer:
+            if self.MaxPlayer == player:
                 if val_of_move > bestvalue:
                     bestvalue = val_of_move
                     bestmove = move
@@ -308,7 +410,7 @@ class game():
             if time() - self.start_time > self.time_limit:
                 break
 
-        if self.MaxPlayer:
+        if self.MaxPlayer == player:
             print('Best move for Max is ' + str(move))
         else:
             print('Best move for Min is ' + str(move))
@@ -323,7 +425,10 @@ if __name__ == "__main__":
     while game.is_winning(game.board) != 10 or game.is_winning(game.board) != -10:
         # Update board with best move of each player
         board = game.do_move(board, game.find_best_move(board, player), player)
+        print('\n\rExplored ' + str(game.no_of_nodes) + ' nodes')
         draw_table_score(board)
+        game.no_of_nodes = 0
+        game.no_of_plies = []
         player = not player
 
 
